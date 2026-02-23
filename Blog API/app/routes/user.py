98d3e2fbccb9ef47d4e144c31ad78a267efa4387
Jamezfn <jamesmakachia19@gmail.com>
2026-02-23 12:36:52 +0300

    Refactor user route implementation; streamline user creation and add get user by ID endpoint

--- Blog API/app/routes/user.py
+++ Blog API/app/routes/user.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 
@@ -11,5 +12,9 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user_data: UserInCreate, db: Session = Depends(get_db)):
     """Create a new user"""
-    service = UserService(session=db)
-    return service.create_user(user_details=user_data)
+    return UserService(session=db).create_user(user_details=user_data)
+
+@router.get("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+def get_user_by_id(user_id: UUID, db: Session = Depends(get_db)):
+    """Get User by id"""
+    return UserService(session=db).get_user_by_id(user_id )
